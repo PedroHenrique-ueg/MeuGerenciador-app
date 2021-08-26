5d6272a5df05e2d981ce2e0edd1f3c1ed414fd1c
PedroHenrique-ueg <84983523+PedroHenrique-ueg@users.noreply.github.com>
2021-08-26 18:39:58 -0300

    as

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { ListarScriptComponent } from './Script/listar-script/listar-script.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'scripts', component: ListarScriptComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

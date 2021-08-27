4ff635fd80be9b585e084ddab129b36913febe60
PedroHenrique-ueg <84983523+PedroHenrique-ueg@users.noreply.github.com>
2021-08-26 21:46:47 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+
 import { CadastrarScriptComponent } from './Script/cadastrar-script/cadastrar-script.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';

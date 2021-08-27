1be1ff5e9459b6afc3ab3eed434b3b52817b7f88
PedroHenrique-ueg <84983523+PedroHenrique-ueg@users.noreply.github.com>
2021-08-26 21:41:38 -0300

    APRIMORAÇÕES

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
-import { ListarScriptComponent } from './Script/listar-script/listar-script.component';
+import { CadastrarScriptComponent } from './Script/cadastrar-script/cadastrar-script.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { ListarScriptComponent } from './Script/listar-script/listar-script.component';
+
 const routes: Routes = [
-  { path: 'scripts', component: ListarScriptComponent }
+  { path: 'scripts', component: ListarScriptComponent },
+  { path: 'home', component: HomeComponent },
+  { path:'novoScript', component: CadastrarScriptComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarScriptComponent } from './Script/listar-script/listar-script.component';
 import { CadastrarScriptComponent } from './Script/cadastrar-script/cadastrar-script.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListarScriptComponent,
-    CadastrarScriptComponent
+    CadastrarScriptComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
